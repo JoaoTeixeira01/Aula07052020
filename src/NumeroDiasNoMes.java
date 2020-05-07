91eb90f7b7f51b00e6bf08367d861625c42ba3a7
MD <mario.duarte@esg.ipsantarem.pt>
2020-05-07 13:00:07 +0100

    Criação bateria testes da classe tools

--- src/NumeroDiasNoMes.java
+++ src/NumeroDiasNoMes.java
@@ -5,17 +5,33 @@ public class NumeroDiasNoMes
 {
 	public static void main(String[] args) 
 	{
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("Introduza uma data no formato: 07/2020");
-		
-		String data = scanner.nextLine();
-		
-		dataObject dataObj = new dataObject(data);
-		
-		int numDias = NumeroDias.devolveNumDias(dataObj.getMes(), dataObj.getAno());
-
-		System.out.println(numDias);
+		try
+		{
+			Scanner scanner = new Scanner(System.in);
+			
+			System.out.println("Introduza uma data no formato: 07/2020");
+			
+			String data = scanner.nextLine();
+			
+			dataObject dataObj = new dataObject(data);
+			
+			int numDias = NumeroDias.devolveNumDias(dataObj.getMes(), dataObj.getAno());
+			
+			String mes = meses.getNomeMes(dataObj.getMes());
+			String ano = String.valueOf(dataObj.getAno());
+			
+			String mensagem= "O mês de " + mes + " do ano " + ano + " tem " + numDias + " dias.";
+	
+			System.out.println(mensagem);
+		}
+		catch(ForaDeParametrosException foraEx)
+		{
+			System.out.println(foraEx.getMessage());
+		}
+		catch(IllegalArgumentException illEx)
+		{
+			System.out.println("Argumento do tipo errado.");
+		}
 	}
 
 }
--- src/com/md/tools.java
+++ src/com/md/tools.java
@@ -9,10 +9,29 @@ public class tools
 		
 		public dataObject(String data)
 		{
-			//"07/2020"
-			String[] mesAno = data.split("/");
-			this.mes = Integer.parseInt(mesAno[0]);
-			this.ano = Integer.parseInt(mesAno[1]);
+			if(data == null)
+			{
+				System.out.print("empty");
+				throw new IllegalArgumentException();
+			}
+			else
+			{
+				//"07/2020"
+				String[] mesAno = data.split("/");
+				if(mesAno.length != 2)
+				{
+					throw new IllegalArgumentException();
+				}
+				else if(Integer.valueOf(mesAno[0]) == null || Integer.valueOf(mesAno[1]) == null)
+				{
+					throw new IllegalArgumentException();
+				}
+				else
+				{
+					this.mes = Integer.parseInt(mesAno[0]);
+					this.ano = Integer.parseInt(mesAno[1]);
+				}
+			}
 		}
 		
 		public int getMes()
@@ -25,9 +44,35 @@ public class tools
 			return this.ano;
 		}
 	}
+	
+	public enum meses
+	{
+		JANEIRO(1), FEVEREIRO(2), MARCO(3), ABRIL(4), MAIO(5), 
+		JUNHO(6), JULHO(7),AGOSTO(8), SETEMBRO(9), OUTUBRO(10),
+		NOVEMBRO(11),DEZEMBRO(12);
+		
+		int nMes;
+		meses(int mes)
+		{
+			nMes = mes;
+		}
+		public static String getNomeMes(int mes) throws ForaDeParametrosException 
+		{
+			int indice = mes - 1;
+			
+			if(indice < 0 || indice > 11)
+			{
+				throw new ForaDeParametrosException("Mês não válido");
+			}
+			meses[] ms = meses.values();
+					
+			return ms[indice].name();
+		}
+	}
+	
 	public static class NumeroDias
 	{
-		public static int devolveNumDias(int mes,int ano)
+		public static int devolveNumDias(int mes,int ano) throws ForaDeParametrosException
 		{
 			int numDiasMes;
 			int[] meses31 = {1,3,5,7,8,10,12};
@@ -51,17 +96,16 @@ public class tools
 			}
 			else
 			{
-				throw new IllegalArgumentException();
+				throw new ForaDeParametrosException("Mês não válido");
 			}
 			return numDiasMes;
 		}
 		
-		private static boolean bissexto(int ano)
+		public static boolean bissexto(int ano)
 		{
 			// ano divisivel por 4 -> bissexto
 			// ano divisivel por 4 e por 100 -> ñ bissexto
 			// ano divisivel por 4, 100 e 400 -> bissexto
-			
 			boolean anoDivisivel4 = divisivel(ano, 4);
 			boolean anoDivisivel100 = divisivel(ano, 100);
 			boolean anoDivisivel400 = divisivel(ano, 400);
@@ -110,6 +154,13 @@ public class tools
 				}
 			}
 			return false;
+		}
+	}
+	public static class ForaDeParametrosException extends Exception
+	{
+		ForaDeParametrosException(String mensagem)
+		{
+			super(mensagem);
 		}
 	}
 }
